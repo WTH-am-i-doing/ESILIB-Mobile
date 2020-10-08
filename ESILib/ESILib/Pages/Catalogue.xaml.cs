430b6f832e1a4a91a82417092ccdf69cdb9c4c88
ZairiAimenDz <zairiaimendev@gmail.com>
2020-10-08 18:08:48 +0100

    Small Changes

--- ESILib/ESILib/Pages/Catalogue.xaml.cs
+++ ESILib/ESILib/Pages/Catalogue.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ESILib.Data;
+using ESILib.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,28 @@ namespace ESILib.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Catalogue : ContentPage
     {
+        private FirebaseHelper helper = new FirebaseHelper();
         public Catalogue()
         {
             InitializeComponent();
+            OnLaunch();
+        }
+        private async void OnLaunch()
+        {
+            try
+            {
+                bookList.ItemsSource = new ObservableCollection<Book>(await helper.GetAllBooks());
+            }
+            catch
+            {
+                await DisplayAlert("Error","Check Your Internet Connection","Ok");
+            }
+        }
+
+        private void RefreshView_Refreshing(object sender, EventArgs e)
+        {
+            OnLaunch();
+            ((RefreshView)sender).IsRefreshing = false;
         }
     }
 }
